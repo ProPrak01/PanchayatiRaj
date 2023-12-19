185d7c4e3965a4e2459b161242c7b78b1e6e392b
BhanuPratap16 <bhanupratap16902@gmail.com>
2023-12-19 20:49:33 +0530

    lgjdflkg

--- Assets/ML model/Classification Script.cs
+++ Assets/ML model/Classification Script.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Unity.Barracuda;
 
 
 public class ClassificationScript : MonoBehaviour
@@ -10,7 +11,8 @@ public class ClassificationScript : MonoBehaviour
     [Header("Model ")]
     public bool ss;
 
-    
+    NNModel model;
+    IWorker work;
     // Start is called before the first frame update
     void Start()
     {
